66e96eca986b6e26c969b54f0d093f7dc53b8a27
Ulrich Wilfried <ulrichwilfried1234@gmail.com>
2024-04-29 02:20:11 +0100

    Image Slider React Done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import ImageSlider from './Components/image-slider/ImageSlider';
+
+function App() {
+
+  return (
+    <>
+    <ImageSlider url={"https://picsum.photos/v2/list"} page={"1"} limit={"10"} />
+    </>
+  )
+}
+
+export default App
